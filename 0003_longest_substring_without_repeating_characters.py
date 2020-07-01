94d21e55f192b2e6138bce41f8f87dd2d4b1f8c5
victorvic54 <sacchirro@yahoo.com>
2020-07-01 14:51:28 +0800

    0003

---
+++ 0003_longest_substring_without_repeating_characters.py
@@ -0,0 +1,55 @@
+
+# First solution
+# Use this example:
+#
+#   1 2 3 4 4 3  -> longest
+#   0 1 2 3 4 5  -> index
+#   a b c e a c
+#
+# Keep track of the longest (max) value for every index. Store it in a dictionary
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        my_dict = dict()
+        
+        result, temp = 0, 0
+        
+        for i in range(len(s)):
+            char = s[i]
+            
+            if (char in my_dict):
+                temp = max(temp, my_dict[char] + 1)
+
+            my_dict[char] = i
+            result = max(result, i - temp + 1)
+            
+        return result
+
+
+# Second solution:
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        start = 0
+        longest = 0
+        my_set = set()
+        
+        for i in range(len(s)):
+            char = s[i]
+            
+            if (char in my_set):
+                if (len(my_set) > longest):
+                    longest = len(my_set)
+                
+                for j in range(start, i):
+                    my_set.remove(s[j])
+                    start += 1
+                    
+                    if (s[j] == char):
+                        break    
+            
+            my_set.add(char)
+            
+        return max(len(my_set), longest)
+            
