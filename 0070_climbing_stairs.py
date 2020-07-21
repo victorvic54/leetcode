3d29d35f0073eb3fae744a86bdd05141cd471dee
victorvic54 <sacchirro@yahoo.com>
2020-07-21 16:01:40 +0800

    0070

---
+++ 0070_climbing_stairs.py
@@ -0,0 +1,8 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        fib_list = [1,1]
+        
+        for i in range(2, n + 1):
+            fib_list.append(fib_list[i - 2] + fib_list[i - 1])
+            
+        return fib_list[n]
