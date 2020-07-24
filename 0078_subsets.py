59a0e0b34eef985c827510cbf14ad47234f18463
victorvic54 <sacchirro@yahoo.com>
2020-07-24 16:03:36 +0800

    0078

---
+++ 0078_subsets.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        nums_len = len(nums)
+        result = []
+        
+        def backtracking(start, tmp_list):
+            if (len(tmp_list) > nums_len):
+                return
+            
+            result.append(tmp_list.copy())
+            
+            for i in range(start, nums_len + 1):
+                backtracking(i + 1, tmp_list + [nums[i-1]])
+        
+        backtracking(1, [])
+        return result
