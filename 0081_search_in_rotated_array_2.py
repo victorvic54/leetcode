b3994a84c8bf4f43cdf02dec90b64f191205dfb1
victorvic54 <sacchirro@yahoo.com>
2020-07-25 16:04:37 +0800

    0081

---
+++ 0081_search_in_rotated_array_2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        lo = 0
+        hi = len(nums) - 1
+        
+        while lo <= hi:
+            mid = lo + (hi - lo) // 2
+
+            if nums[mid] == target:
+                return True
+            
+            while lo < mid and nums[lo] == nums[mid]: # tricky part
+                lo += 1
+            
+            # the first half is ordered
+            if nums[lo] <= nums[mid]:
+                # target is in the first half
+                if nums[lo] <= target < nums[mid]:
+                    hi = mid - 1
+                else:
+                    lo = mid + 1
+            # the second half is ordered
+            else:
+                # target is in the second half
+                if nums[mid] < target <= nums[hi]:
+                    lo = mid + 1
+                else:
+                    hi = mid - 1
+        
+        return False
