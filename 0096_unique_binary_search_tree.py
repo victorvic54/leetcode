daa116f14a3bd9b1b087ba7b91be9c475580be77
victorvic54 <sacchirro@yahoo.com>
2020-08-05 01:54:02 +0800

    0096

---
+++ 0096_unique_binary_search_tree.py
@@ -0,0 +1,28 @@
+'''
+
+Catalan number:
+n = 0  -> 0
+n = 1  -> 1
+n = 2  -> c0 * c1 + c1 * c0 
+n = 3  -> c0 * c2 + c1 * c1 + c2 * c0
+n = 4  -> c0 * c3 + c1 * c2 + c2 * c1 + c3 * c0
+
+'''
+
+class Solution:
+    def numTrees(self, n: int) -> int:
+        memo = [0] * (n + 1)
+        memo[0] = 1
+        memo[1] = 1
+        
+        def num_tree_helper(num):
+            if (memo[num] != 0):
+                return memo[num]
+            else:
+                for i in range(num):
+                    memo[num] += num_tree_helper(i) * num_tree_helper(num - i - 1)
+                
+                return memo[num]
+            
+        num_tree_helper(n)
+        return memo[n]
