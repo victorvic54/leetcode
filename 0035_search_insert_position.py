a46630f42b55b803be692fa8b68e4d1431eb870c
Victor Varian <sacchirro@yahoo.com>
2023-02-20 21:54:56 +0800

    add #0035

--- 0035_search_insert_position.py
+++ 0035_search_insert_position.py
@@ -1,7 +1,26 @@
+# O(log n) solution
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
+        low, high = 0, len(nums)
+        
+        while low < high:
+            mid = (low + high) // 2
+            if target > nums[mid]:
+                low = mid + 1
+            else:
+                high = mid
+
+        return low
+
+
+# O(n) solution
+class solution:
+    def searchinsert(self, nums: list[int], target: int) -> int:
         for i in range(len(nums)):
             if (target <= nums[i]):
                 return i
         
         return len(nums)
+
+
+
