a80ad33d7d7324e74e56a0c320ce12b4aab7e2b7
Victor Varian <sacchirro@yahoo.com>
2023-03-30 12:32:03 +0800

    #0087

--- 0087_scramble_string.py
+++ 0087_scramble_string.py
@@ -1,5 +1,12 @@
 class Solution:
     def isScramble(self, s1, s2):
+        self.memo = {}
+        return self.scrambleHelper(s1, s2)
+
+    def scrambleHelper(self, s1, s2):
+        if (s1, s2) in self.memo:
+            return self.memo[(s1, s2)]
+
         if len(s1) != len(s2):
             return False
         
@@ -10,8 +17,11 @@ class Solution:
             return False
         
         for i in range(1, len(s1)):
-            if (self.isScramble(s1[:i], s2[:i]) and self.isScramble(s1[i:], s2[i:])) or \
-               (self.isScramble(s1[:i], s2[-i:]) and self.isScramble(s1[i:], s2[:-i])):
+            if (self.scrambleHelper(s1[:i], s2[:i]) and self.scrambleHelper(s1[i:], s2[i:])) or \
+               (self.scrambleHelper(s1[:i], s2[-i:]) and self.scrambleHelper(s1[i:], s2[:-i])):
+                self.memo[(s1, s2)] = True
                 return True
+
+            self.memo[(s1, s2)] = False
 
         return False
