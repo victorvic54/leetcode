4fcaa75dbd3d7b6558712e2ae2b3209de8d6e6b4
Victor Varian <sacchirro@yahoo.com>
2023-04-06 01:31:28 +0800

    #2439

---
+++ 2439_minimize_maximum_of_array.py
@@ -0,0 +1,9 @@
+answer = 0
+        prefix_sum = 0
+        
+        # Iterate over nums, update prefix sum and answer.
+        for i in range(len(nums)):
+            prefix_sum += nums[i]
+            answer = max(answer, math.ceil(prefix_sum / (i + 1)))
+
+        return answer
