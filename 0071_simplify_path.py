03c267d9a54cb9382c5b9028a11e5780d26e21e2
Victor Varian <sacchirro@yahoo.com>
2023-04-12 20:38:15 +0800

    #0071

--- 0071_simplify_path.py
+++ 0071_simplify_path.py
@@ -1,7 +1,6 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        stack = []
-        
+        stack = []        
         path_list = path.split("/")
         
         for path in path_list:
