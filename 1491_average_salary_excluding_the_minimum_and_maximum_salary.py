928233a26524dc1d2820ab513c5fb9bfe426788f
Victor Varian <sacchirro@yahoo.com>
2023-05-01 19:12:35 +0800

    #1491

---
+++ 1491_average_salary_excluding_the_minimum_and_maximum_salary.py
@@ -0,0 +1,16 @@
+class Solution:
+    def average(self, salary: List[int]) -> float:
+        total = 0
+        min_val = float(inf)
+        max_val = 0
+
+        for val in salary:
+            total += val
+
+            if val > max_val:
+                max_val = val
+            
+            if val < min_val:
+                min_val = val
+        
+        return (total - min_val - max_val) / (len(salary) - 2)
