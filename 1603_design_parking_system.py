4d7785b83bea7e911341d1d5144c346ffee0dfa9
Victor Varian <sacchirro@yahoo.com>
2023-05-29 21:06:58 +0800

    #1603

---
+++ 1603_design_parking_system.py
@@ -0,0 +1,19 @@
+class ParkingSystem:
+    def __init__(self, big: int, medium: int, small: int):
+        # Number of empty slots for each type of car
+        self.empty = [big, medium, small]
+
+    def addCar(self, carType: int) -> bool:
+        # If space is available, allocate and return True
+        if self.empty[carType - 1] > 0:
+            self.empty[carType - 1] -= 1
+            return True
+
+        # Else, return False
+        return False
+        
+
+
+# Your ParkingSystem object will be instantiated and called as such:
+# obj = ParkingSystem(big, medium, small)
+# param_1 = obj.addCar(carType)
