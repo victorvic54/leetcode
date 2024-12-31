afb931f3f05c47a59d0ce82b5f371a324eb721d7
victorvic54 <sacchirro@yahoo.com>
2024-12-31 12:53:27 +0800

    #0983

--- 0983_minimum_cost_for_tickets.py
+++ 0983_minimum_cost_for_tickets.py
@@ -1,25 +1,35 @@
-# greedy algorithm using DP
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-        self.days = days
-        self.dp = [0] * (len(days) + 1)
+        traveldays = set(days)
+        dp = [0] * 366
+        for i in range(1, 366):
+            if i not in traveldays:
+                dp[i] = dp[i-1]
+            else:
+                dp[i] = min(
+                    dp[i-1] + costs[0], 
+                    dp[i-7] + costs[1] if i >= 7 else costs[1], 
+                    dp[i-30] + costs[2] if i >= 30 else costs[2])
+        return dp[-1]
 
-        for i in range(1, len(self.dp)):
-            self.dp[i] = min(
-                self.get_lowest_cost(i, 1) + costs[0],
-                self.get_lowest_cost(i, 7) + costs[1],
-                self.get_lowest_cost(i, 30) + costs[2],
-            )
-        return self.dp[-1]
-    
-    # remember curr_idx is index + 1 for self.dp but not for self.days
-    def get_lowest_cost(self, curr_idx, days_before):
-        curr_day = self.days[curr_idx - 1]
 
-        while curr_idx > 0:
-            curr_idx -= 1
-            if curr_day - self.days[curr_idx - 1] >= days_before:
-                break
-        
-        return self.dp[curr_idx]]
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        memo = {}
+        def backtracking(idx, covered):
+            if idx >= len(days):
+                return 0
 
+            if days[idx] <= covered:
+                return backtracking(idx + 1, covered)
+            
+            if (idx, covered) in memo:
+                return memo[(idx, covered)]
+            
+            min_val = float('inf')
+            for i in range(len(costs)):
+                curr_cost = costs[i] + backtracking(idx + 1, days[idx] + day_cost_len[i] - 1)
+                min_val = min(min_val, curr_cost)
+
+            memo[(idx, covered)] = min_val
+            return min_val
+        return backtracking(0, 0)
