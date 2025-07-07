d7d519fa31aa830c237cc1ad2c08c2f8ef0f3ea2
victorvic54 <sacchirro@yahoo.com>
2025-07-07 21:50:49 +0800

    #1353

---
+++ 1353_maximum_number_of_events_that_can_be_attended.py
@@ -0,0 +1,26 @@
+"""
+Time Complexity: O((n + T) log n)
+Space Complexity: O(n)
+"""
+class Solution:
+    def maxEvents(self, events: List[List[int]]) -> int:
+        n = len(events)
+        max_day = max([event[1] for event in events])
+        events.sort()
+
+        pq = []
+        ans = 0
+        j = 0
+        for i in range(1, max_day + 1): # the pq can be at most total n pushes and deletion
+            while j < n and events[j][0] <= i:
+                heapq.heappush(pq, events[j][1])
+                j += 1
+
+            while pq and pq[0] < i:
+                heapq.heappop(pq)
+
+            if pq:
+                heapq.heappop(pq)
+                ans += 1
+
+        return ans
